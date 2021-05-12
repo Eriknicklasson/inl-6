4adba142d6e8a14e55b3ba91e8980765b76a251e
Eriknicklasson <62430975+Eriknicklasson@users.noreply.github.com>
2021-05-12 16:48:18 +0200

    Add files via upload
    Min lösning på uoogiften

---
+++ App.js
@@ -0,0 +1,13 @@
+import MovieList from './MovieList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Filmer</h1>
+      <h3> Lägg till en film: </h3>
+      <MovieList />
+    </div>
+  );
+}
+
+export default App;
